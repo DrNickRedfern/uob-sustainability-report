1111a0f37586400cb00b88f5a009c6a8960fb53a
DrNickRedfern <71695111+DrNickRedfern@users.noreply.github.com>
2024-05-29 21:07:02 +0100

    updated benchmarking

--- sdg_benchmarks.py
+++ sdg_benchmarks.py
@@ -1,20 +1,27 @@
+# Adapted from https://api-lab.dimensions.ai/cookbooks/8-organizations/7-benchmarking-organizations.html
+
 import dimcli
 import pandas as pd
 
 import os
 
+# Set search parameters
 YEAR: int = 2022
+
+# Set paths
+HOME_DIR: str = os.path.dirname(os.getcwd())
+DATA_DIR: str = os.path.join(HOME_DIR, 'data')
+    
+# Log into Dimensions and get data
+dimcli.login()
+dsl = dimcli.Dsl()
 
 result_sdg = dsl.query(f"""
                     search publications
                     where year = {YEAR}
                     return category_sdg limit 1000""").as_dataframe()
 
-result_sdg['level'] = result_sdg.name.apply(lambda n: len(n.split(' ')[0]))
-# result = result.assign(year = YEAR)
-# result = result[['year'] + [x for x in result.columns if x != 'year']]
-
-result_sdg['cutoff'] = (result_sdg['count'] * .01)#.astype('int')
+result_sdg['cutoff'] = (result_sdg['count'] * .01).astype('int')
 
 dfl = []
 
@@ -35,6 +42,12 @@ for r in result_sdg.iterrows():
     result['name'] = r[1]['name']
     result['id'] = r[1]['id']
     dfl.append(result)
-    
+
+# Tidy up the data and export
 cutoffs = pd.concat(dfl)
-cutoffs.to_csv('1_percent_cutoffs_sdg.csv', index=False)
+cutoffs[['sdg_code','sdg_name']] = cutoffs['name'].str.split(pat=' ', n=1, expand=True)
+cutoffs = cutoffs.drop(columns=['id', 'name'])
+cutoffs = cutoffs[[col for col in cutoffs.columns if col != 'field_citation_ratio'] + ['field_citation_ratio']]
+
+outfile = ''.join(['sdg_benchmarks_', str(YEAR), '.csv'])
+cutoffs.to_csv(os.path.join(DATA_DIR, outfile), index=False)
